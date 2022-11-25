37717510f5286749c2276ab1868f37371a1224b4
ivyhy98 <ivanahy98@gmail.com>
2022-11-24 21:42:25 -0500

    added views

--- server.js
+++ server.js
@@ -1,10 +1,23 @@
+require('dotenv').config();
 const express = require('express');
+const reactViews = require("express-react-views");
 const app = express();
 const port = 3000;
 
 
-app.get('/',(req,res)=>{
-    res.send('new');
+//View engine setup
+app.set('view engine', 'jsx');
+app.engine('jsx', reactViews.createEngine());
+
+//Middleware setup
+app.use((req,res,next)=>{
+    next();
+});
+
+app.use(express.urlencoded({extended:false}));
+
+app.get('/new',(req,res)=>{
+    res.render('New');
 });
 app.listen(port,()=>{
     console.log('listening on port', port);
---
+++ views/New.jsx
@@ -0,0 +1,19 @@
+const React = require('react');
+
+class New extends React.Component{
+    render(){
+
+        return (
+          <div>
+            <h1>Captains log</h1>
+            <form action="" method="post">
+                Title: <input type="text" name="title" /> <br />
+                Log: <input type="text" name="entry" /> <br />
+                Is the ship Broken? Yes:<input type="checkbox" name="shipIsBroken" /> No: Ignore <br />
+                <input type="submit" value="Submit Log" />
+            </form>
+          </div>
+        );
+    }
+}
+module.exports = New;
