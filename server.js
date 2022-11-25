bc1cb3c389ce1e6d2a4d9d97ee3baae913733a6a
ivyhy98 <ivanahy98@gmail.com>
2022-11-24 21:54:33 -0500

    added create route

--- server.js
+++ server.js
@@ -16,9 +16,17 @@ app.use((req,res,next)=>{
 
 app.use(express.urlencoded({extended:false}));
 
-app.get('/new',(req,res)=>{
+app.get('/logs',(req,res)=>{
+    res.send('home')
+})
+
+app.get('/logs/new',(req,res)=>{
     res.render('New');
 });
+
+app.post('/logs',(req,res)=>{
+    res.send(req.body);
+})
 app.listen(port,()=>{
     console.log('listening on port', port);
 });
--- views/New.jsx
+++ views/New.jsx
@@ -6,7 +6,7 @@ class New extends React.Component{
         return (
           <div>
             <h1>Captains log</h1>
-            <form action="" method="post">
+            <form action="/logs" method="post">
                 Title: <input type="text" name="title" /> <br />
                 Log: <input type="text" name="entry" /> <br />
                 Is the ship Broken? Yes:<input type="checkbox" name="shipIsBroken" /> No: Ignore <br />
